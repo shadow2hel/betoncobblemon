0b0109b617b8d4e5e377ddff39619c6a10c62e98
Shadow2hel <antonio.carrano@live.be>
2023-04-30 16:18:59 +0200

    Added BattleVictoryData to make already defeated players persistent through restarts

--- src/main/java/shadow2hel/betoncobblemon/event/BattleVictoryObjective.java
+++ src/main/java/shadow2hel/betoncobblemon/event/BattleVictoryObjective.java
@@ -8,26 +8,25 @@ import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
 import kotlin.Unit;
 import org.betonquest.betonquest.Instruction;
 import org.betonquest.betonquest.api.profiles.OnlineProfile;
+import org.betonquest.betonquest.api.profiles.Profile;
 import org.betonquest.betonquest.exceptions.InstructionParseException;
 import org.betonquest.betonquest.utils.PlayerConverter;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import shadow2hel.betoncobblemon.util.PokeSelector;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class BattleVictoryObjective extends PokeObjective {
     private ActorType typeOfEnemy;
-    private Set<UUID> defeatedPlayers;
 
     public BattleVictoryObjective(final Instruction instruction) throws InstructionParseException {
         super(instruction, new PokeSelector(instruction.next()));
+        template = BattleVictoryData.class;
         targetAmount = instruction.getVarNum();
         Optional<String> enemyType = instruction.getOptionalArgument("type");
         typeOfEnemy = null;
-        defeatedPlayers = new HashSet<>();
         if (enemyType.isPresent()) {
             try {
                 typeOfEnemy = ActorType.valueOf(enemyType.get().toUpperCase());
@@ -56,26 +55,31 @@ public class BattleVictoryObjective extends PokeObjective {
                                 .forEach(onlineProfile -> {
                                     Arrays.stream(loserSide.getActors())
                                             .forEach(loserActor -> {
+                                                BattleVictoryData battleData = (BattleVictoryData) getCountingData(onlineProfile);
+                                                Set<UUID> alreadyDefeated = ((BattleVictoryData) getCountingData(onlineProfile)).getDefeatedPlayers();
                                                 UUID newPlayer = StreamSupport.stream(loserActor.getPlayerUUIDs().spliterator(), false)
-                                                        .filter(loserUUID -> loserActor.getType() == ActorType.PLAYER && !defeatedPlayers.contains(loserUUID))
+                                                        .filter(loserUUID -> loserActor.getType() == ActorType.PLAYER
+                                                                && !alreadyDefeated.contains(loserUUID))
                                                         .findFirst()
                                                         .orElse(null);
 
                                                 if (event.getBattle().isPvP() && newPlayer != null) {
-                                                    defeatedPlayers.add(newPlayer);
-                                                    loserActor.getPokemonList().stream()
-                                                            .map(BattlePokemon::getOriginalPokemon)
-                                                            .forEach(loserPokemon -> {
-                                                                if (containsPlayer(onlineProfile) && pokeSelector.matches(loserPokemon) && checkConditions(onlineProfile)) {
-                                                                    handleDataChange(onlineProfile, getCountingData(onlineProfile).add());
-                                                                }
-                                                            });
+                                                    boolean isUnknownPlayer = battleData.tryProgress(newPlayer);
+                                                    if (isUnknownPlayer) {
+                                                        loserActor.getPokemonList().stream()
+                                                                .map(BattlePokemon::getOriginalPokemon)
+                                                                .forEach(loserPokemon -> {
+                                                                    if (containsPlayer(onlineProfile) && pokeSelector.matches(loserPokemon) && checkConditions(onlineProfile)) {
+                                                                        handleDataChange(onlineProfile, battleData.add());
+                                                                    }
+                                                                });
+                                                    }
                                                 } else if(!event.getBattle().isPvP()) {
                                                     loserActor.getPokemonList().stream()
                                                             .map(BattlePokemon::getOriginalPokemon)
                                                             .forEach(loserPokemon -> {
                                                                 if (containsPlayer(onlineProfile) && pokeSelector.matches(loserPokemon) && checkConditions(onlineProfile)) {
-                                                                    handleDataChange(onlineProfile, getCountingData(onlineProfile).add());
+                                                                    handleDataChange(onlineProfile, battleData.add());
                                                                 }
                                                             });
                                                 }
@@ -95,5 +99,35 @@ public class BattleVictoryObjective extends PokeObjective {
     protected void handleDataChange(final OnlineProfile onlineProfile, final CountingData data) {
         final String message = "pokemon_to_defeat";
         completeIfDoneOrNotify(onlineProfile, message);
+    }
+
+    public static class BattleVictoryData extends CountingData {
+        private final Set<UUID> defeatedPlayers;
+        public BattleVictoryData(String instruction, Profile profile, String objID) {
+            super(instruction, profile, objID);
+            defeatedPlayers = new HashSet<>();
+            final String[] pokesInstruction = instruction.split(";", 3);
+            if (pokesInstruction.length >= 2 && !pokesInstruction[1].isEmpty()) {
+                Arrays.stream(pokesInstruction[1].split("/"))
+                        .map(UUID::fromString)
+                        .forEach(defeatedPlayers::add);
+            }
+        }
+
+        public boolean tryProgress(final UUID player) {
+            final boolean added = defeatedPlayers.add(player);
+            if (added)
+                progress();
+            return added;
+        }
+
+        public Set<UUID> getDefeatedPlayers() {
+            return defeatedPlayers;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + ";" + defeatedPlayers.stream().map(UUID::toString).collect(Collectors.joining("/"));
+        }
     }
 }
